a5a41805c01e9487744da8470e19dc147c4e437d
Danny Smith <danny.smith@anaplan.com>
2021-12-02 18:36:40 +0000

    Update bootstrap sql: - fix multi row insert - create app user with only necessary permissions

--- bootstrap.sql
+++ bootstrap.sql
@@ -1,4 +1,7 @@
 CREATE DATABASE pave_test;
 USE pave_test;
 CREATE TABLE ping_pong (id VARCHAR(4), value VARCHAR(4));
-INSERT INTO ping_pong VALUES (('ping', 'pong'), ('pong', 'ping'));
+INSERT INTO ping_pong VALUES ('ping', 'pong'), ('pong', 'ping');
+CREATE USER 'app'@'*' IDENTIFIED BY 'supersecurepassword';
+GRANT SELECT ON pave_test.ping_pong TO 'app'@'%';
+FLUSH PRIVILEGES;
